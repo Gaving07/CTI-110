73c466164272b6d9aa11033c55be428c9820abd5
Gaving07 <guyg6975@student.faytechcc.edu>
2026-09-09 11:03:53 -0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,6 @@
+#Gavin Guy
+#9/9/2026
+#Lab1
+user_1stname = input("Enter your first name: ")
+user_2ndname = input("Enter your last name: ")
+print("Hello,", user_1stname, user_2ndname + "!", "Welcome to CTI-110")
