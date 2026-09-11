b1150228e3077a92a3f43bbc6af202a196f46751
Gaving07 <guyg6975@student.faytechcc.edu>
2026-09-11 12:11:47 -0400

    Add files via upload

---
+++ P1HW1_GuyGavin.py
@@ -0,0 +1,23 @@
+# Gavin Guy
+# September 11, 2026
+# P1HW1
+# This program performs calculations using user-entered integers.
+
+base = int(input("Enter an integer for the base: "))
+exponent = int(input("Enter an integer for the exponent: "))
+
+result = base ** exponent
+
+print()
+print(f"{base} raised to the power of {exponent} is {result}!!")
+
+print()
+
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter another integer: "))
+num3 = int(input("Enter a third integer: "))
+
+result2 = num1 + num2 - num3
+
+print()
+print(f"{num1} + {num2} - {num3} is {result2}!!")
